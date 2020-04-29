d8cbb0650fbcebe0eab14808da845faaedc86e63
Carlos Poveda <c.poveda@outlook.com>
2020-04-28 23:22:35 -0600

    minor fix

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const targets = document.querySelectorAll("img");
 
 const lazyLoad = (target) => {
   const io = new IntersectionObserver((entries, observer) => {
-    console.log(entries);
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         const img = entry.target;
