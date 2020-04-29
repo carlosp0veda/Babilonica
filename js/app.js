cda57f241709af34c6a7e70c175cb7eb7ced92cb
Carlos Poveda <c.poveda@outlook.com>
2020-04-28 18:14:34 -0600

    fix lazy load

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
+// lazy loading
+
+const targets = document.querySelectorAll("img");
+
+const lazyLoad = (target) => {
+  const io = new IntersectionObserver((entries, observer) => {
+    console.log(entries);
+    entries.forEach((entry) => {
+      console.log("😍");
+
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const src = img.getAttribute("data-lazy");
+
+        img.setAttribute("src", src);
+        img.classList.add("fade");
+
+        observer.disconnect();
+      }
+    });
+  });
+
+  io.observe(target);
+};
+
+targets.forEach(lazyLoad);
+
 // scrolling banner
 
 var bannerStatus = 1;
