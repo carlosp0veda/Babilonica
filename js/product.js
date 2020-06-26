0f3b142c9895bd7d459fbc6d25aa07a2e56ce90f
Carlos Poveda <c.poveda@outlook.com>
2020-06-26 01:59:01 -0600

    image zoom

--- js/product.js
+++ js/product.js
@@ -28,6 +28,7 @@ function displayProduct(plantas) {
               <div class="side-photo" onclick="changePhoto(event, '${displayProduct.img3}')"><img class="photo-s" src=${displayProduct.img3} alt="${displayProduct.title}"></div>
             </div>
             <div id="main-photo">
+            <div id="lens"></div>
                 <img class="photo-m" src=${displayProduct.img} alt="${displayProduct.title}">
             </div>
           </section>
@@ -94,26 +95,4 @@ function displayProduct(plantas) {
   }
 
   document.getElementById("open").click();
-
-  //   Product gallery
-}
-
-function openTab(evt, tabName) {
-  // Declare all variables
-
-  // Get all elements with class="tabcontent" and hide them
-  const tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  const tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active-tab", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active-tab";
 }
