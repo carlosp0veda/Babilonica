ce2d5a91804eca0e4194e423af4af4c45c84acb0
Carlos Poveda <c.poveda@outlook.com>
2020-07-26 23:43:31 -0600

    minor data.js fixes

--- js/data.js
+++ js/data.js
@@ -387,7 +387,7 @@ const data = [
   },
   {
     id: 27,
-    title: "Monstera Adansonii + Serenito",
+    title: "Monstera Adansonii",
     url: "monstera_adansonii.html",
     category: "ornamental",
     price: "$40",
